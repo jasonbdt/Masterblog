31e0943ef9f573772c739dbb18b1b2f8005f7216
jasonbdt <ja.bladt@gmail.com>
2025-10-28 15:53:34 +0100

    Create index route

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from typing import Optional
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index() -> Optional[str]:
+    try:
+        with open("data/posts.json", "r") as file_obj:
+            blog_posts = file_obj.read()
+    except FileNotFoundError:
+        print("No posts")
+    else:
+        return render_template('index.html', posts=blog_posts)
+
+    return None
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
