51472bb099010d518672b43ff501bb53539cb506
jasonbdt <ja.bladt@gmail.com>
2025-10-28 22:19:12 +0100

    Add method to handle requests to /delete route

--- app.py
+++ app.py
@@ -21,8 +21,10 @@ def add() -> Union[str, Response]:
         author = request.form.get('author')
         content = request.form.get('content')
 
-        blog_posts: list[dict] = utils.get_blog_posts()
-        post_exist = list(filter(lambda post: post['title'] == title, blog_posts))
+        blog_posts = utils.get_blog_posts()
+        post_exist = list(
+            filter(lambda post: post['title'] == title, blog_posts)
+        )
 
         if post_exist:
             print("Error") # TODO: Return error that post already exists
@@ -40,6 +42,26 @@ def add() -> Union[str, Response]:
             return redirect(url_for('index'))
 
     return render_template('add.html')
+
+
+@app.route('/delete/<int:post_id>')
+def delete(post_id: int) -> Union[str, Response]:
+    blog_posts: list[dict] = utils.get_blog_posts()
+    post_to_delete = list(
+        filter(lambda post: post['id'] == post_id, blog_posts)
+    )
+
+    if post_to_delete:
+        blog_posts.remove(post_to_delete[0])
+        print(f'Blog post with ID {post_id} deleted.')
+        utils.save_posts(blog_posts)
+
+        return redirect(url_for('index'))
+
+    return render_template(
+        'index.html',
+        posts=blog_posts,
+        form_error=f"No post found with ID {post_id}")
 
 
 if __name__ == '__main__':
